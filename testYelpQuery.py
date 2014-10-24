93d970e258ae1cd6314c84ef6841ebd466f5d6e1
Justin Roll <justin.j.roll@gmail.com>
2014-10-24 15:05:35 -0700

    Added a getFeatures to the YelpQuery API. Started pulling reviewCounts and bucketing both the ratings and reviewCounts

--- testYelpQuery.py
+++ testYelpQuery.py
@@ -3,9 +3,11 @@ from yelpQuery import YelpQuery
 def testQuery():
     yelpQuery = YelpQuery()
     yelpQuery.connect()
-    response = yelpQuery.search(term='ice cream', location = "San Luis Obispo", limit = 3)
-    print(response)
-
+    response = yelpQuery.search(term='Jaffa Cafe', location = "San Luis Obispo", limit = 3)
+    print(response["businesses"][0]["rating"])
+    print(response["businesses"][0]["name"])
+    print(response["businesses"][0]) 
+    print(yelpQuery.getFeatures("Jaffa Cafe", "San Luis Obispo"))
 def main():
     testQuery()
 
--- yelpQuery.py
+++ yelpQuery.py
@@ -5,9 +5,6 @@ import sys
 import urllib
 from yelpapi import YelpAPI
 
-
-
-
 class YelpQuery:
 
     def __init__(self):
@@ -32,3 +29,69 @@ class YelpQuery:
     def search(self, **kwargs):
         search_results = self.yelp_api.search_query(**kwargs)
         return search_results
+    
+    #get a valid restaurant 
+    def getValidRestaurant(self, **kwargs):
+        response = self.search(**kwargs)
+        nameMatch = False
+        result = None 
+        i = 0
+        while not nameMatch and i < 3:
+            business = response["businesses"][i]
+            if business["name"].lower() == kwargs["term"].lower():
+                nameMatch = True
+                result = business
+            i+=1
+        return result
+    
+    def getFeatures(self, restaurantName, location):
+        featureDict = {}
+        yelpRestaurant = self.getValidRestaurant(term = restaurantName, location = location, limit = 3)
+        if yelpRestaurant:
+            featureDict["rating"] = self.getRating(yelpRestaurant)
+            featureDict["reviewCount"] = self.getReviews(yelpRestaurant)
+        return featureDict
+
+    def getReviews(self, restaurant):
+        reviewCount = restaurant["review_count"]
+        
+
+        if reviewCount >= 500:
+            reviewCountBucket = "above500"
+        elif reviewCount >= 400:
+            reviewCountBucket = "above400"
+        elif reviewCount >= 300:
+            reviewCountBucket = "above300"
+        elif reviewCount >= 200:
+            reviewCountBucket = "above200"
+        elif reviewCount >= 100:
+            reviewCountBucket = "above100"
+        else:
+            reviewCountBucket = "below100"
+    
+        return reviewCountBucket
+    
+    def getRating(self, restaurant):
+        rating = restaurant["rating"]
+
+        if rating >= 4.5:
+            ratingBucket = "above4.5"
+
+        elif rating >= 4:
+            ratingBucker = "above4"
+
+        elif rating >= 3.5:
+            ratingBucket = "above3.5"
+
+        elif rating >= 3.0:
+            ratingBucket = "above3"
+
+        elif rating >= 2.0:
+            ratingBucket = "above2"
+
+        elif rating >= 1.0:
+            ratingBucket = "above1"
+        else:
+            ratingBucket = "below1"
+
+        return ratingBucket 
