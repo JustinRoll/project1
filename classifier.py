d0f289d1c0aacc743e0bce4178fba05c3ef69ede
Justin Roll <justin.j.roll@gmail.com>
2014-11-13 20:31:21 -0800

    Implemented polarity features for the exact score classifier

--- classifier.py
+++ classifier.py
@@ -7,13 +7,19 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords, wordnet
 from nltk.stem.snowball import SnowballStemmer
 from yelpQuery import YelpQuery
+from sentencePolarity import SentencePolarity
 
+#to do: add features for word sentiments
+    #high amount of positive sentiment sentences
+    #anything to be done for exact scores?
+    #add rmse stuff
 class Classifier:
 
     def __init__(self, reviews):
         self.reviews = reviews
         self.yelpQuery = YelpQuery()
         self.yelpQuery.connect()
+        self.polarity = SentencePolarity('twitter_sentiment_list.csv')
     
     def getBucket(self, count):
         if count <= 0:
@@ -26,22 +32,39 @@ class Classifier:
         return returnCount
     
     def getOverallFeatures(self, doc):
-
+         
         featureDict = {}
         wordDict, topicWordDict, sents, stemmedWordDict = self.extractReviewWords(doc)
         wordList = sorted([word.lower() for word in set(stemmedWordDict.keys()) if word not in stopwords.words('English') and word not in ',-.;();$' and word not in '-' and word not in '.'],
                 key = lambda x : stemmedWordDict[x], reverse=True)
         top10WordList = [wordList[i] for i in range(0, 10 if len(wordList) > 10 else len(wordList) - 1)]
+        positiveSentenceCount = 0
+        negativeSentenceCount = 0
 
+        for sent in sents:
+        
+            tokenizedSent = [word for word in word_tokenize(sent) if ',' not in word and '.' not in word]
+            negProb, posProb = self.polarity.classifySentiment(tokenizedSent)
+            if posProb >= .6:
+                positiveSentenceCount += 1
+            if negProb >= .6:
+                negativeSentenceCount += 1
 
-        #for sent in sents:
-        #        tokenizedSent = [word for word in word_tokenize(sent) if ',' not in word and '.' not in word]
-        #        wordTrigrams = trigrams(tokenizedSent)
+            for word in tokenizedSent:
+                posProb, negProb = self.polarity.classifyWord(word)
+                if negProb >= .8:
+                    featureDict[word + " neg polarity"] = 1
+                if posProb >= .8:
+                    featureDict[word + " pos polarity"] = 1 
+
+#        wordTrigrams = trigrams(tokenizedSent)
         #        for trigram in wordTrigrams:
         #                featureDict[trigram] = True
-        for unigram in wordList:
-            featureDict[unigram] = 1#self.getBucket(stemmedWordDict[unigram])
-        weight = 1
+#        for unigram in wordList:
+#            featureDict[unigram] = 1#self.getBucket(stemmedWordDict[unigram])
+        
+        featureDict["posSentences"] = self.getBucket(positiveSentenceCount)
+        featureDict["negSentences"] = self.getBucket(negativeSentenceCount)
         #yelpFeatures = self.yelpQuery.getFeatures(doc.name, doc.city, weight)
         #featureDict.update(yelpFeatures)
         return featureDict
@@ -60,13 +83,15 @@ class Classifier:
                 for trigram in wordTrigrams:
                         featureDict[trigram] = True
         for unigram in top10WordList:
-            featureDict[unigram] = self.getBucket(stemmedWordDict[unigram])
+            featureDict[unigram] = 1#featureDict[unigram] = self.getBucket(stemmedWordDict[unigram])
 
         return featureDict 
 
     def getParagraphFeaturesExact(self, doc):
-
         featureDict = {}
+        positiveSentenceCount = 0
+        negativeSentenceCount = 0        
+
         wordDict, topicWordDict, sents, stemmedWordDict = self.extractReviewWordsFromParagraph(doc)
         wordList = sorted([word.lower() for word in set(stemmedWordDict.keys()) if word.lower() not in stopwords.words('English') and word not in ',-.;();$' and word not in '-' and word not in '.'],
                 key = lambda x : stemmedWordDict[x], reverse=True)
@@ -74,18 +99,33 @@ class Classifier:
 
         for sent in sents:
                 tokenizedSent = [word for word in word_tokenize(sent) if ',' not in word and '.' not in word]
-                wordTrigrams = trigrams(tokenizedSent)
-                for trigram in wordTrigrams:
-                        featureDict[trigram] = True
+                
+                posProb, negProb = self.polarity.classifySentiment(tokenizedSent)
+                if posProb >= .6:
+                    positiveSentenceCount += 1
+                if negProb >= .6:
+                    negativeSentenceCount += 1
+                for word in tokenizedSent:
+                    negProb, posProb = self.polarity.classifyWord(word)
+                    if negProb >= .8:
+                        featureDict[word + " neg polarity"] = 1
+                    if posProb >= .8:
+                        featureDict[word + " pos polarity"] = 1
+                #wordTrigrams = trigrams(tokenizedSent)
+                #for trigram in wordTrigrams:
+                #        featureDict[trigram] = True
                 #wordBigrams = bigrams(tokenizedSent)
                 #for bigram in wordBigrams:
                 #        featureDict[bigram] = True
                 #fourGrams = ngrams(tokenizedSent, 4)
                 #for fourGram in fourGrams:
                 #        featureDict[fourGram] = True
-        for unigram in topWordList:
-            featureDict[unigram] = self.getBucket(stemmedWordDict[unigram])
-        
+        #for unigram in topWordList:
+        #    featureDict[unigram] = self.getBucket(stemmedWordDict[unigram])
+
+
+        featureDict["posSentences"] = self.getBucket(positiveSentenceCount)
+        featureDict["negSentences"] = self.getBucket(negativeSentenceCount) 
         return featureDict   
 
 
@@ -170,7 +210,7 @@ class Classifier:
         classifier = nltk.NaiveBayesClassifier.train(train)
         print(classifier.show_most_informative_features(20))
  
-        return rmse.getError(classifier, test)#nltk.classify.accuracy(classifier,test) 
+        return rmse.getError(classifier, test), nltk.classify.accuracy(classifier,test) 
 
     def classifyOverallReviewsExact(self):
 
@@ -184,7 +224,7 @@ class Classifier:
         classifier = nltk.NaiveBayesClassifier.train(train)
         print(classifier.show_most_informative_features(20))
  
-        return nltk.classify.accuracy(classifier,test)  
+        return rmse.getError(classifier, test), nltk.classify.accuracy(classifier,test)  
 
 
     def classifyParagraphReviewsExact(self):
@@ -203,7 +243,7 @@ class Classifier:
         classifier = nltk.NaiveBayesClassifier.train(train)
         print(classifier.show_most_informative_features(20))
  
-        return nltk.classify.accuracy(classifier,test)  
+        return rmse.getError(classifier, test), nltk.classify.accuracy(classifier,test)  
 
     def classifyAuthorshipReviews(self):
         
@@ -233,6 +273,7 @@ class Classifier:
                     docs.append((paragraph, 1))
                 else:
                     docs.append((paragraph, 0))
+        
         random.shuffle(docs)
 
         featureSets = [(self.getParagraphFeatures(d),label) for (d, label) in docs]
@@ -242,7 +283,7 @@ class Classifier:
         classifier = nltk.NaiveBayesClassifier.train(train)
         print(classifier.show_most_informative_features(20))
 
-        return rmse.getError(classifier, test)#nltk.classify.accuracy(classifier,test)  
+        return rmse.getError(classifier, test), nltk.classify.accuracy(classifier,test)  
  
     def getAverages(self):
         runningTotal = 0.0
@@ -250,13 +291,3 @@ class Classifier:
             runningTotal += self.classifyOverallReviews()
 
         print("Overall Positive/Negative prediction accuracy: %f", runningTotal / 5)
-
-
-    def compareSynsets(self, word):
-        posSynsets = wordnet.synsets("positive")
-        negSynsets = wordnet.synsets("negative")
-        wordSynsets = wordnet.synsets(word)
-        
-        print(wordSynsets[0].path_similarity(posSynsets[0]))
-        print(wordSynsets[0].path_similarity(negSynsets[0]))
-               
--- driver.py
+++ driver.py
@@ -5,7 +5,9 @@ from review import *
 def main():
     reviews = importReviews("./reviews")
     classifier = Classifier(reviews)
-    print(classifier.classifyAuthorshipReviews())
-    print(classifier.classifyOverallReviews())
-    print(classifier.classifyParagraphReviews())
+    #print("Paragraph rmse: %f, accuracy: %f" % (classifier.classifyParagraphReviews()))
+    print("Exact Score (0-5) Paragraph rmse: %f, accuracy: %f" % (classifier.classifyParagraphReviewsExact()))
+    #print("Overall rmse: %f, accuracy: %f" % (classifier.classifyOverallReviews()))
+    print("Exact Overall rmse: %f, accuracy: %f" % (classifier.classifyOverallReviewsExact()))
+    #print("Authorship score: %f" % classifier.classifyAuthorshipReviews())
 main() 
